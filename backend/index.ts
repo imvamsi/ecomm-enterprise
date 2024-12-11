265946f7635e54c3419a5258be7aa201c4f6b40a
Vamsi Krishna <krrishvamsi7@gmail.com>
2024-12-11 16:15:48 -0500

    fixed cors issue on localhost

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import products from "./data/products.js";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(cors());
 const PORT = process.env.SERVER_PORT;
 app.get("/", (req: Request, res: Response) => {
   res.send("helloworld aarya");
 });
 
 app.get("/api/products/:id", (req, res) => {
-  console.log("req obj is", req);
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(products);
 });
 
 app.listen(PORT, () => {
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
 import products from "../products";
+import Product from "./Product";
 
 export default function Home(): JSX.Element {
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    const data = await axios.get("http://localhost:8000/api/products");
+    console.log(data);
+  }
   return (
     <>
       <h1>LatestProducts</h1>
