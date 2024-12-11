d6ea6bceabbbec8c02593b80a03f29d413cef5d3
Vamsi Krishna <krrishvamsi7@gmail.com>
2024-12-11 15:15:41 -0500

    added product json to test

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import products from "./data/products.js";
 
 const app: Express = express();
 const PORT = 8000;
 app.get("/", (req: Request, res: Response) => {
   res.send("helloworld aarya");
+});
+
+app.get("/api/products/:id", (req, res) => {
+  console.log("req obj is", req);
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(PORT, () => {
