f5fde6519529c3527be66e76538099bd704847a6
Vamsi Krishna <krrishvamsi7@gmail.com>
2024-12-16 12:50:43 -0500

    added product model and ts interface

---
+++ backend/models/product.model.ts
@@ -0,0 +1,103 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IReview extends Document {
+  user: mongoose.Types.ObjectId;
+  name: string;
+  rating: number;
+  comment: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface IProduct extends Document {
+  user: mongoose.Types.ObjectId;
+  name: string;
+  image: string;
+  brand: string;
+  category: string;
+  description: string;
+  reviews: IReview[];
+  rating: number;
+  price: number;
+  countInStock: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const reviewSchema = new Schema<IReview>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = new Schema<IProduct>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Review = mongoose.model<IReview>("Review", reviewSchema);
+const Product = mongoose.model<IProduct>("Product", productSchema);
+
+export { Review, Product };
