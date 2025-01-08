d34e3081fe00525f110156cb0a9ae63ba1bdf67f
Vamsi Krishna <krrishvamsi7@gmail.com>
2025-01-08 15:50:43 -0500

    cart design part 1 complete

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,6 +1,71 @@
 import { Link, useNavigate } from "react-router-dom";
-import { UseDispatch } from "react-redux";
+import { UseDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  Form,
+  Card,
+  Image,
+  Button,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
+import { FaTrash } from "react-icons/fa";
+import Message from "../components/Message";
 
 export default function Cart() {
-  return <div>cart</div>;
+  const { cartItems } = useSelector((state) => state.cart);
+  return (
+    <Row>
+      <Col md={8}>
+        {<h1 style={{ marginBottom: "1rem" }}> Shopping Cart</h1>}
+        {cartItems.length === 0 ? (
+          <Message>
+            {" "}
+            Your Cart is empty <Link to={"/"}>Start Shopping</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems &&
+              cartItems.map((item) => (
+                <ListGroupItem key={item._id}>
+                  <Row>
+                    <Col md={3}>
+                      <Image src={item.image} fluid rounded />
+                    </Col>
+                    <Col md={3} className="align-center">
+                      <Link to={`/product/${item._id}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={2} className="align-center">
+                      {item.price}
+                    </Col>
+                    <Col md={2} className="align-center">
+                      <Form.Control
+                        as="select"
+                        value={item.qty}
+                        onChange={(e) => console.log(e)}
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col md={2} className="align-center">
+                      <Button
+                        variant="danger"
+                        onClick={() => console.log(item._id)}
+                      >
+                        <FaTrash />
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+              ))}
+          </ListGroup>
+        )}
+      </Col>
+    </Row>
+  );
 }
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import ProductDetails from "../pages/ProductDetails";
-import Cart from "../pages/cart";
+import Cart from "../pages/Cart";
 
 function AppRoutes() {
   return (
