1e420ba4250bee293698699132ec42106485277b
Vamsi Krishna <krrishvamsi7@gmail.com>
2025-01-14 14:37:02 -0500

    added http only cookie and login with jwt

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,23 @@
+import { strict } from "assert";
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 const authenticateUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "365d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
     res.json({
       _id: user._id,
       name: user.name,
