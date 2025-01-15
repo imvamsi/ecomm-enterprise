9c60df8e3521dd36b7eab65b42770d68c579c322
Vamsi Krishna <krrishvamsi7@gmail.com>
2025-01-15 15:17:25 -0500

    Added auth slice and login reducer fn

---
+++ frontend/src/slices/authSlice.tsx
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: localStorage.getItem("credentials")
+    ? JSON.parse(localStorage.getItem("credentials"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("credentials", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
