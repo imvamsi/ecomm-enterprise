0c3cb89819e59c2cebf6ebd4fe748f4484167459
Vamsi Krishna <krrishvamsi7@gmail.com>
2025-01-15 11:42:24 -0500

    added multiple controller functions and endpoints verified

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -65,12 +65,67 @@ const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Logged out successfully!!" });
 });
 
+const getUserProfile = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const existingUser = await User.findById(userId);
+  if (existingUser) {
+    res.json({
+      _id: existingUser._id,
+      name: existingUser.name,
+      email: existingUser.email,
+      isAdmin: existingUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      //user.password = req.body.password;
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      user.password = hashedPassword;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("update failed: user not found");
+  }
+});
+
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get user");
+  const users = await User.find({}).select("-password");
+  if (users.length > 0) {
+    res.json(users);
+  } else {
+    res.status(404);
+    throw new Error("No user found!!");
+  }
 });
 
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("get user by id");
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 const updateUser = asyncHandler(async (req, res) => {
@@ -79,14 +134,6 @@ const updateUser = asyncHandler(async (req, res) => {
 
 const deleteUser = asyncHandler(async (req, res) => {
   res.send("delete user");
-});
-
-const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
-});
-
-const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update user ppro");
 });
 
 export {
