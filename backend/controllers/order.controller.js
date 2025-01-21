55eaf3a0a60ce516706fca922f23430f108b2758
Vamsi Krishna <krrishvamsi7@gmail.com>
2025-01-21 12:24:34 -0500

    added create order controller

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,34 +1,83 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import Order from "../models/order.model.js";
+import ProductModel from "../models/product.model.js";
+import {calcPrices} from "../utils/calcPrice.js";
+import OrderModel from "../models/order.model.js";
+
 
 const createOrder = asyncHandler(async (req, res) => {
-  res.send("create order");
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod
+    } = req.body
+    if (orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error('orderItem not found')
+    }
+
+    const itemsFromDb = await ProductModel.find({
+        _id: {$in: orderItems.map((item) => item._id)},  // Conditions
+    });
+
+    //map over order items and get the price from the db and restructure the order object
+
+    const dbOrderItems = orderItems.map(itemFromClient => {
+        const matchingItem = itemsFromDb.find(x => x._id === itemFromClient._id)
+        return {
+            ...itemFromClient,
+            product: itemFromClient._id,
+            price: matchingItem.price,
+            _id: undefined
+        }
+    })
+
+    const {
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice
+    } = calcPrices(dbOrderItems);
+
+    const order = new OrderModel({
+        orderItems: dbOrderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder)
+
 });
 
 const getCurrentUserOrders = asyncHandler(async (req, res) => {
-  res.send("get current order of user");
+    res.send("get current order of user");
 });
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("order is paid");
+    res.send("order is paid");
 });
 
 const markAsDelivered = asyncHandler(async (req, res) => {
-  res.send("order delivered");
+    res.send("order delivered");
 });
 
 const getAllOrders = asyncHandler(async (req, res) => {
-  res.send("all orders");
+    res.send("all orders");
 });
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("order by id");
+    res.send("order by id");
 });
 
 export {
-  createOrder,
-  getCurrentUserOrders,
-  updateOrderToPaid,
-  markAsDelivered,
-  getAllOrders,
-  getOrderById,
+    createOrder,
+    getCurrentUserOrders,
+    updateOrderToPaid,
+    markAsDelivered,
+    getAllOrders,
+    getOrderById,
 };
